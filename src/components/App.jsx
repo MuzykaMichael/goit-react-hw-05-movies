14b62278d9ff187b7c8dc33f78be9492350f433b
Muzyka Mykhailo <Muzyka_Mishel@ukr.net>
2023-07-23 15:38:21 +0300

    microUpdate

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,12 +2,14 @@ import {  Routes, Route, Navigate } from "react-router-dom";
 import { Header } from "./Header/Header";
 import { lazy } from "react";
 
+const HomePage = lazy(()=>import('../Pages/HomePage/HomePage'));
+const SearchMovie = lazy(()=>import('../Pages/SearchMovie/SearchMovie'))
+const MovieDetails = lazy(()=>import('../Pages/MovieDetails/MovieDetails'));
+const Cast = lazy(()=>import('../components/Cast/Cast'));
+const Reviews = lazy(()=>import('../components/Reviews/Reviews'));
+
 export const App = () => {
-  const HomePage = lazy(()=>import('../Pages/HomePage/HomePage'));
-  const SearchMovie = lazy(()=>import('../Pages/SearchMovie/SearchMovie'))
-  const MovieDetails = lazy(()=>import('../Pages/MovieDetails/MovieDetails'));
-  const Cast = lazy(()=>import('../components/Cast/Cast'));
-  const Reviews = lazy(()=>import('../components/Reviews/Reviews'));
+
 
   return (
       <>
